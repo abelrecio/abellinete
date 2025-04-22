9908d291d2cd3c1bbbbe1e8343c0eb48aeeb05b8
Abel Recio Municio <abrecio-@c1r13s6.42madrid.com>
2025-04-22 16:59:26 +0200

    mocos 2

--- test_files/memcmp.c
+++
@@ -1,7 +0,0 @@
-memcmp:abc,abc,3
-memcmp:abc,abd,2
-memcmp:abc,ab,3
-memcmp:, ,1
-memcmp:123,123,3
-memcmp:123,124,3
-memcmp:short,long,3
--- test_files/memmove.c
+++
@@ -1,6 +0,0 @@
-memmove:,xyz,0
-memmove:,xyz,3
-memmove:hello,world,5
-memmove:foo,bar,3
-memmove:abcdef,ghi,4
-memmove:overlap,erlap,7
